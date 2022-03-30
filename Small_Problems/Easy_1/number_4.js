d5920db1fa60f44b952ab53da9d5ac4a3d07fc39
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-03-30 19:02:21 -0400

    Added features to number 4

--- Small_Problems/Easy_1/number_4.js
+++ Small_Problems/Easy_1/number_4.js
@@ -1,10 +1,27 @@
 const readline = require(`readline-sync`);
 const SQMETERS_TO_SQFEET = 10.7639;
+const SQFEET_TO_SQMETERS = 1 / SQMETERS_TO_SQFEET;
 
-console.log(`Enter the length of the room in meters:`);
+console.log(`Would you rather have the calculation done in square meters or square feet? Enter meters or feet.`);
+let userInput = readline.question();
+
+while (userInput !== 'meters' && userInput !== 'feet') {
+  console.log(`That is not a valid input. Please try again: meters or feet.`);
+  userInput = readline.question();
+}
+
+console.log(`Enter the length of the room in ${userInput}:`);
 let lengthOfRoom = Number(readline.question());
 
-console.log(`Enter the width of the room in meters:`);
+console.log(`Enter the width of the room in ${userInput}:`);
 let widthOfRoom = Number(readline.question());
 
-console.log(`The area of the room is ${(lengthOfRoom * widthOfRoom).toFixed(2)} square meters (${(lengthOfRoom * widthOfRoom * SQMETERS_TO_SQFEET).toFixed(2)} square feet).`);
+if (userInput === 'meters') {
+
+  console.log(`The area of the room is ${(lengthOfRoom * widthOfRoom).toFixed(2)} square meters (${(lengthOfRoom * widthOfRoom * SQMETERS_TO_SQFEET).toFixed(2)} square feet).`);
+
+} else {
+
+  console.log(`The area of the room is ${(lengthOfRoom * widthOfRoom).toFixed(2)} square feet (${(lengthOfRoom * widthOfRoom * SQFEET_TO_SQMETERS).toFixed(2)} square meters).`);
+
+}
