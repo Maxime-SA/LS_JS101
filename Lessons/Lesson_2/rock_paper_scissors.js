5138993135a6e2700728b5552f0e84ac892a9f8d
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-03-30 01:29:37 -0400

    Completed the RPS Bonus Features

--- Lessons/Lesson_2/rock_paper_scissors.js
+++ Lessons/Lesson_2/rock_paper_scissors.js
@@ -1,23 +1,36 @@
 const readline = require(`readline-sync`);
-const VALID_CHOICES = [`rock`, `paper`, `scissors`];
+
+const USER_CHOICES = [`rock`, `paper`, `scissors`, `lizard`, `spock`];
+const VALID_CHOICES = [`r`, `p`, `sc`, `l`, `sp`];
+
+const WINNING_COMBINATIONS = {
+  r: [`l`, `sc`],
+  p: [`sp`, 'r'],
+  sc: [`p`, 'l'],
+  sp: [`r`, 'sc'],
+  l: [`sp`, 'p'],
+};
 
 function prompt(message) {
   console.log(`==> ${message}\n`);
 }
 
-function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice} and the computer chose ${computerChoice}`);
+function registerWinner(choice, computerChoice, humanArray, computerArray) {
 
-  if ((choice === `rock` && computerChoice === `scissors`) ||
-      (choice === `paper` && computerChoice === `rock`) ||
-      (choice === `scissors` && computerChoice === `paper`)) {
+  if (WINNING_COMBINATIONS[choice].includes(computerChoice)) {
+
     prompt(`You win!`);
-  } else if ((choice === `rock` && computerChoice === `paper`) ||
-            (choice === `paper` && computerChoice === `scissors`) ||
-            (choice === `scissors` && computerChoice === `rock`)) {
-    prompt(`Computer wins!`);
+    humanArray.push('W');
+
+  } else if (choice === computerChoice) {
+
+    prompt(`It's a tie`);
+
   } else {
-    prompt(`It's a tie.`);
+
+    prompt(`Computer wins!`);
+    computerArray.push('W');
+
   }
 }
 
@@ -25,21 +38,35 @@ let playAgain;
 
 do {
 
-  prompt(`Choose one: ${VALID_CHOICES.join(`, `)}`);
-  let choice = readline.question();
+  let humanScore = [];
+  let computerScore = [];
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt(`That's not a valid choice`);
-    choice = readline.question();
+  do {
+
+    prompt(`Choose one: ${USER_CHOICES.map(string => (string[0] !== 's' ? string + ` (` + string[0] + `)` : string + ` (` + string.slice(0,2) + `)`)).join(`, `)}`);
+    let choice = readline.question();
+
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt(`That's not a valid choice`);
+      choice = readline.question();
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    registerWinner(choice, computerChoice, humanScore, computerScore);
+
+    prompt(`You have ${humanScore.length} point(s) and the computer has ${computerScore.length} point(s)`);
+
+  } while (humanScore.length < 3 && computerScore.length < 3);
+
+  if (humanScore.length > computerScore.length) {
+    prompt(`You won the best of 5!`);
+  } else {
+    prompt(`The computer won the best of 5!`);
   }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  displayWinner(choice, computerChoice);
 
   prompt(`Do you wish to play again? Answer yes or no.`);
   playAgain = readline.question().toUpperCase();
 
 } while (playAgain === 'YES');
-
