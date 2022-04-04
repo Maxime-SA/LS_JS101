036da2611065cdfbb014d0fabd999c70257d0288
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-04 17:35:40 -0400

    Completed number 10

---
+++ Small_Problems/Easy_5/number_10.js
@@ -0,0 +1,8 @@
+function average(array) {
+
+  return Math.floor(array.reduce((previous, current) => previous + current, 0) / array.length);
+
+}
+
+console.log(average([1,5,87,45,8,8]));
+console.log(average([9,47,23,95,16,52]));
