55e40a448ee037ed23f0a3f7ada5f8ca9136c289
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-05 19:47:55 -0400

    Completed practice problem 9

---
+++ Lessons/Lesson_5/practice_problem_9.js
@@ -0,0 +1,15 @@
+let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
+
+
+let newArray = arr.map(array => {
+
+  if (typeof a === Number) {
+    return array.slice().sort((a,b) => a - b);
+  } else {
+    return array.slice().sort();
+  }
+
+});
+
+console.log(arr);
+console.log(newArray);
