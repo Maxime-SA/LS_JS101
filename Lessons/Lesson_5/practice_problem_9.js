8e79d3f9776db62830e9377b04ee3c914e3cf376
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-05 19:50:56 -0400

    Completed practice problem 9

--- Lessons/Lesson_5/practice_problem_9.js
+++ Lessons/Lesson_5/practice_problem_9.js
@@ -3,7 +3,7 @@ let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
 
 let newArray = arr.map(array => {
 
-  if (typeof a === Number) {
+  if (typeof array[0] === 'number') {
     return array.slice().sort((a,b) => a - b);
   } else {
     return array.slice().sort();
