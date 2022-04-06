a20a693c7914e13d9ebd9377800e475930afb020
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-06 11:01:13 -0400

    Completed number 3

---
+++ Small_Problems/Easy_6/number_3.js
@@ -0,0 +1,9 @@
+function reverseNumber(number) {
+  return Number(String(number).split('').reverse().join(''));
+}
+
+console.log(reverseNumber(12345));
+console.log(reverseNumber(12213));
+console.log(reverseNumber(456));
+console.log(reverseNumber(12000));
+console.log(reverseNumber(1));
