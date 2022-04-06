bf5c48dd9b2a96dc076c2eeeb85f4f6d7486b6d9
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-06 11:36:20 -0400

    Completed number 9

---
+++ Small_Problems/Easy_6/number_9.js
@@ -0,0 +1,9 @@
+function reverseSentence(string) {
+
+  return string.split(' ').reverse().join(' ');
+
+}
+
+console.log(reverseSentence(''));
+console.log(reverseSentence('Hello World'));
+console.log(reverseSentence('Reverse these words'));
