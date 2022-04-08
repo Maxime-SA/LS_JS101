b04a0e80660e37f46e0fd730ef0a2dc9e5f17c1f
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-08 16:45:29 -0400

    Completed number 2

---
+++ Small_Problems/String_and_Text_Processing/number_2.js
@@ -0,0 +1,15 @@
+function removeVowels(arr) {
+
+  let newArray = arr.map(element => {
+
+    return element.replace(/[aeiou]/gi, '');
+
+  });
+
+  return newArray;
+
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
