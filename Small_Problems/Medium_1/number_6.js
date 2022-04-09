8472c715cf44b377ad54851f0be4ded4f55412bf
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-09 12:10:19 -0400

    Small fix

--- Small_Problems/Medium_1/number_6.js
+++ Small_Problems/Medium_1/number_6.js
@@ -1,16 +1,16 @@
-function fibonnacci(number) {
+function fibonacci(number) {
 
   if (number === 0) return 0;
   if (number === 1) return 1;
 
-  return fibonnacci(number - 1) + fibonnacci(number - 2);
+  return fibonacci(number - 1) + fibonacci(number - 2);
 
 }
 
-console.log(fibonnacci(1));
-console.log(fibonnacci(2));
-console.log(fibonnacci(3));
-console.log(fibonnacci(4));
-console.log(fibonnacci(5));
-console.log(fibonnacci(12));
-console.log(fibonnacci(20));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(12));
+console.log(fibonacci(20));
