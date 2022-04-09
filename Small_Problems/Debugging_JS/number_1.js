e4a284c60ab7539fdad0e21d3bcc8b7201358596
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-09 19:31:56 -0400

    Completed number 1

---
+++ Small_Problems/Debugging_JS/number_1.js
@@ -0,0 +1,13 @@
+let ladder = ''
+
+['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
+  if (ladder !== '') {
+    ladder += '-'
+  }
+
+  ladder += word
+})
+
+console.log(ladder)
+
+// The error occurs here because we didn't end the declaration and initialization of the ladder variable with a semi-colon
