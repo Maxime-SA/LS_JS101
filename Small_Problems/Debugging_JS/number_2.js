bebe863f97b7b3c83d1afa6b0911832ebd72d749
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-09 19:35:46 -0400

    Completed number 2

---
+++ Small_Problems/Debugging_JS/number_2.js
@@ -0,0 +1,28 @@
+
+const RESERVED_KEYWORDS = ['break', 'case', 'catch', 'class', 'const', 'continue',
+  'debugger', 'default', 'delete', 'do', 'else', 'enum', 'export', 'extends', 'finally',
+  'for', 'function', 'if', 'implements', 'import', 'in', 'instanceof', 'interface',
+  'let', 'new', 'package', 'private', 'protected', 'public', 'return', 'static',
+  'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while',
+  'with', 'yield'];
+
+function isReserved(name) {
+
+  let usedWord = 0;
+
+  RESERVED_KEYWORDS.forEach(reserved => {
+
+    if (name === reserved) {
+      usedWord += 1;
+    }
+
+  });
+
+  return usedWord > 0;
+
+}
+
+console.log(isReserved('monkey')); // false
+console.log(isReserved('patch'));  // false
+console.log(isReserved('switch')); // should be: true
+
