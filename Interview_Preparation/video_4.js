e3d0cff21c540bbfc3382d708e9129819fead443
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-13 14:49:51 -0400

    Completed the second problem from the fourth video

--- Interview_Preparation/video_4.js
+++ Interview_Preparation/video_4.js
@@ -1,3 +1,5 @@
+// First Problem
+
 function commonPrefix(myArray) {
 
   let allPrefixes = [];
@@ -47,3 +49,32 @@ console.log(commonPrefix(['dog','racecar','car']));
 console.log(commonPrefix(['interspecies','interstellar','interstate']));
 console.log(commonPrefix(['throne','dungeon']));
 console.log(commonPrefix(['throne','throne']));
+
+// Second Problem
+
+function findEvenIndex(array) {
+
+  for (let i = 0; i < array.length; i++) {
+
+    let leftSide = array.slice(0,i);
+    let rightSide = array.slice(i + 1);
+
+    if (sumArray(leftSide) === sumArray(rightSide)) return i;
+
+  }
+
+  return -1;
+
+}
+
+function sumArray(array) {
+  return array.reduce((prev, curr) => prev + curr, 0);
+}
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
+console.log(findEvenIndex([1,100,50,-51,1,1]));
+console.log(findEvenIndex([1,2,3,4,5,6]));
+console.log(findEvenIndex([20,10,30,10,10,15,35]));
+console.log(findEvenIndex([20,10,-80,10,10,15,35]));
+console.log(findEvenIndex([10,-80,10,10,15,35,20]));
+console.log(findEvenIndex([-1,-2,-3,-4,-3,-2,-1]));
