60946bdedfdcb832ef9d3dcbf0e61f4ae984d886
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-04-14 12:43:30 -0400

    Fixed the code to match the answers

--- Interview_Preparation/practice_problem_3.js
+++ Interview_Preparation/practice_problem_3.js
@@ -1,25 +1,31 @@
 function toWeirdCase(string) {
 
-  let oldWords = string.split(' ');
-  let newWords = [];
+  let array = string.split(' ');
 
-  oldWords.forEach((element, index) => {
+  return array.map((element, index) => {
 
-    let condition = (index % 2 !== 0) && element.length >= 4;
-
-    if (condition) {
-      element = element.slice(0,3) + element[3].toUpperCase() + element.slice(4);
+    if (index % 2 !== 0 && element.length >= 4) {
+      element = element.split('').map((char, indx) => {
+        if ((indx + 1) % 4 === 0) {
+          char = char.toUpperCase();
+        }
+        return char;
+      }).join('');
     }
 
-    newWords.push(element);
+    return element;
 
-  });
-
-  return newWords.join(' ');
+  }).join(' ');
 
 }
 
-console.log(toWeirdCase('Lorem Ipsum is simply dummy text of the printing world'));
-console.log(toWeirdCase('It is a long established fact that a reader will be distracted'));
-console.log(toWeirdCase('aaA bB c'));
-console.log(toWeirdCase('Miss Mary Poppins word is supercalifragilisticexpialidocious'));
+console.log(
+  toWeirdCase('Lorem Ipsum is simply dummy text of the printing world') ===
+              'Lorem IpsUm is simPly dummy texT of the printing worLd');
+console.log(
+  toWeirdCase('It is a long established fact that a reader will be distracted') ===
+              'It is a lonG established facT that a reader wilL be disTracTed');
+console.log(toWeirdCase('aaA bB c') === 'aaA bB c');
+console.log(
+  toWeirdCase('Miss Mary Poppins word is supercalifragilisticexpialidocious') ===
+              'Miss MarY Poppins worD is supErcaLifrAgilIstiCexpIaliDociOus');
